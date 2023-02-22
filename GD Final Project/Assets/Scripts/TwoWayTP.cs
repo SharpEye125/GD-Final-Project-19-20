1a2072745bf97e8d6d2d7b17a0ff9016db23c2e9
SharpEye125 <creeperminer125@gmail.com>
2023-02-22 13:20:23 -0800

    Added TP offset to 2WayTP + TPDelay

--- GD Final Project/Assets/Scripts/TwoWayTP.cs
+++ GD Final Project/Assets/Scripts/TwoWayTP.cs
@@ -7,6 +7,10 @@ public class TwoWayTP : MonoBehaviour
     public Transform point;
     public Transform player;
     public float inputRange = 2f;
+    public Vector2 tpOffset = new Vector2(0, -0.8f);
+    public float tpDelay = 0.5f;
+    float timer;
+    PlatformerMove pm;
 
     // Start is called before the first frame update
     void Start()
@@ -17,18 +21,22 @@ public class TwoWayTP : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        pm = player.GetComponent<PlatformerMove>();
+        timer += Time.deltaTime;
         Vector2 distance = new Vector2(transform.position.x - player.position.x, transform.position.y - player.position.y);
-        if (distance.magnitude <= inputRange && Input.GetKeyDown(KeyCode.E) && player.gameObject.GetComponent<PlatformerMove>().grounded == true && player.gameObject.GetComponent<PlayerDie>().dead != true)
+        if (distance.magnitude <= inputRange && Input.GetKeyDown(KeyCode.E) && pm.grounded == true && player.gameObject.GetComponent<PlayerDie>().dead != true && timer >= tpDelay)
         {
-            player.position = point.position;
+            player.position = point.position + new Vector3(tpOffset.x, tpOffset.y, point.position.z);
             Debug.Log("TPing to point 2");
+            timer = 0;
             return;
         }
         Vector2 pointDistance = new Vector2(point.position.x - player.position.x, point.position.y - player.position.y);
-        if (pointDistance.magnitude <= inputRange && Input.GetKeyDown(KeyCode.E) && player.gameObject.GetComponent<PlatformerMove>().grounded == true && player.gameObject.GetComponent<PlayerDie>().dead != true)
+        if (pointDistance.magnitude <= inputRange && Input.GetKeyDown(KeyCode.E) && pm.grounded == true && player.gameObject.GetComponent<PlayerDie>().dead != true && timer >= tpDelay)
         {
-            player.position = transform.position;
+            player.position = transform.position + new Vector3(tpOffset.x, tpOffset.y, transform.position.z);
             Debug.Log("TPing to point 1");
+            timer = 0;
         }
     }
 }
