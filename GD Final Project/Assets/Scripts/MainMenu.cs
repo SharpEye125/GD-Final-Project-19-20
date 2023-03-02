202afc5d638dbd2ba5488ecc1c77862f36a30a2a
SharpEye125 <51964791+SharpEye125@users.noreply.github.com>
2023-03-02 13:57:42 -0800

    Main Menu Start Game button now puts you in the tutorial
    and the Continue game temporarily puts you directly in the Hub room. And changed "version" to ver 0.2a

--- GD Final Project/Assets/Scripts/MainMenu.cs
+++ GD Final Project/Assets/Scripts/MainMenu.cs
@@ -24,7 +24,7 @@ public class MainMenu : MonoBehaviour
     }
     public void ContinueGame()
     {
-        //SceneManager.LoadScene(lastLevel);
+        SceneManager.LoadScene(2);
     }
     public void QuitGame()
     {
