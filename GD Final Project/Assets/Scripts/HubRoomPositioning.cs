c3f843b04e2be431d74c9942e19e44914968512e
SharpEye125 <51964791+SharpEye125@users.noreply.github.com>
2023-03-06 13:05:20 -0800

    Wrote working Hubroom Start Position script
    Other changes include: particle changes to slime room and added look to player script to the hovering enemies in Hubroom

--- GD Final Project/Assets/Scripts/HubRoomPositioning.cs
+++ GD Final Project/Assets/Scripts/HubRoomPositioning.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class HubRoomPositioning : MonoBehaviour
 {
+    public static Vector3 startPosition;
+    public static string temp;
+    public static Vector3 offset = new Vector3(0, -0.5f, 0);
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (SceneManager.GetActiveScene().buildIndex == 2 && startPosition != new Vector3(0,0,0))
+        {
+            transform.position = startPosition + offset;
+            Debug.Log("Started at " + temp);
+        }
+        else
+        {
+            Debug.Log("No Start Point set");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            startPosition = new Vector3(0, 0, 0);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Finish")
+        {
+            startPosition = collision.transform.position;
+            temp = collision.name;
+            Debug.Log("Start point " + temp + " has been set.");
+        }
     }
 }
