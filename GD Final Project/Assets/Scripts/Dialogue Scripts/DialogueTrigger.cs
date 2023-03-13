625aec248689676530e6d6f1fa814fc18d7e5aca
SharpEye125 <51964791+SharpEye125@users.noreply.github.com>
2023-03-13 13:43:49 -0700

    Added first interact/talk functionality to DialogueTrigger
    Also made a simple script to set the sprite of a sprite light to the parent of the sprite light.

--- GD Final Project/Assets/Scripts/Dialogue Scripts/DialogueTrigger.cs
+++ GD Final Project/Assets/Scripts/Dialogue Scripts/DialogueTrigger.cs
@@ -4,18 +4,32 @@ using UnityEngine;
 
 public class DialogueTrigger : MonoBehaviour
 {
-
+    [Tooltip("If Enabled: Utilizing firstTalkDialogue's sentences, the character will have an inital set of sentences to speak when first spoked to by the player.")]
+    public bool firstInteractDialogue;
+    public bool interacted;
     public Dialogue dialogue;
+    [Tooltip("The non sentence variables will automatically be set to the normal dialogue variables.")]
+    public Dialogue firstTalkDialogue;
     
 
     public void TriggerDialogue()
     {
-        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+        if (firstInteractDialogue == false || interacted == true)
+        {
+            FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+        }
+        else if (interacted == false)
+        {
+            interacted = true;
+            FindObjectOfType<DialogueManager>().StartDialogue(firstTalkDialogue);
+        }
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+        firstTalkDialogue.name = dialogue.name;
+        firstTalkDialogue.portrait = dialogue.portrait;
+        firstTalkDialogue.voice = dialogue.voice;
     }
 
     // Update is called once per frame
