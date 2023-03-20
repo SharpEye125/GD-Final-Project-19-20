0c6ecc658f8145b8b2b7dcd3dd04289e9d20ae74
SharpEye125 <51964791+SharpEye125@users.noreply.github.com>
2023-03-20 12:56:11 -0700

    Added particle VFX to Beholder Laser

--- GD Final Project/Assets/Scripts/BeholderLaser.cs
+++ GD Final Project/Assets/Scripts/BeholderLaser.cs
@@ -8,10 +8,16 @@ public class BeholderLaser : MonoBehaviour
     public Transform firePoint;
     public Transform tempTarget;
     public LayerMask groundLayer;
+    public GameObject startFX;
+    public GameObject endFX;
+
+    private Quaternion rotation;
+    private List<ParticleSystem> particles;
     //public LayerMask playerLayer;
     // Start is called before the first frame update
     void Start()
     {
+        FillLists();
         EnableLaser();
     }
 
@@ -24,17 +30,30 @@ public class BeholderLaser : MonoBehaviour
     void EnableLaser()
     {
         line.enabled = true;
+        for(int i=0; i<particles.Count; i++)
+        {
+            particles[i].Play();
+        }
     }
     void DisableLaser()
     {
         line.enabled = false;
+        for (int i = 0; i < particles.Count; i++)
+        {
+            particles[i].Stop();
+        }
     }
     void UpdateLaser()
     {
         line.SetPosition(0, firePoint.position);
+        startFX.transform.position = (Vector2)firePoint.position;
         Vector2 direction = tempTarget.position - transform.position;
         RaycastHit2D hit = Physics2D.Raycast(transform.position, direction.normalized, direction.magnitude, groundLayer);
         line.SetPosition(1, hit.point);
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        rotation.eulerAngles = new Vector3(0, 0, angle);
+        transform.rotation = rotation;
 
         //RaycastHit2D hitPlayer = Physics2D.Raycast(transform.position, direction.normalized, direction.magnitude, playerLayer);
         if (hit)
@@ -50,8 +69,25 @@ public class BeholderLaser : MonoBehaviour
                 }
             }
         }
+        endFX.transform.position = line.GetPosition(1);
 
-
+    }
+    void FillLists()
+    {
+        for(int i=0; i < startFX.transform.childCount; i++)
+        {
+            var ps = startFX.transform.GetChild(i).GetComponent<ParticleSystem>();
+            if (ps != null)
+                particles.Add(ps);
+        }
+        for (int i = 0; i < endFX.transform.childCount; i++)
+        {
+            var ps = endFX.transform.GetChild(i).GetComponent<ParticleSystem>();
+            if (ps != null)
+            {
+                particles.Add(ps);
+            }
+        }
     }
     
 }
